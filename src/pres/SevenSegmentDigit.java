f59f081ede8020d4d17711336287c4b76c586249
Katarzyna Rak <katarzynarak9@gmail.com>
2024-06-08 13:25:24 +0200

    Seven segment digit component

--- src/pres/SevenSegmentDigit.java
+++ src/pres/SevenSegmentDigit.java
@@ -6,8 +6,9 @@ import java.awt.*;
 public class SevenSegmentDigit extends JPanel {
     int value;
     public SevenSegmentDigit(){
-        this.setPreferredSize(new Dimension(30,60));
-        value = 0;
+        this.setPreferredSize(new Dimension(42,60));
+        value = 7;
+        setBackground(Color.BLACK);
     }
     void setValue(int value){
         if(value >= 0 && value <= 9){
@@ -18,5 +19,66 @@ public class SevenSegmentDigit extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.RED);
+        if(value == 0){
+            g.fillRect(12,3,20,3);
+            g.fillRect(6,8,3,20);
+            g.fillRect(35,8,3,20);
+            g.fillRect(35,33,3,20);
+            g.fillRect(6,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 1){
+            g.fillRect(35,8,3,20);
+            g.fillRect(35,33,3,20);
+        }else if(value == 2){
+            g.fillRect(12,3,20,3);
+            g.fillRect(35,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(6,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 3){
+            g.fillRect(12,3,20,3);
+            g.fillRect(35,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 4){
+            g.fillRect(6,8,3,20);
+            g.fillRect(35,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+        }else if(value == 5){
+            g.fillRect(12,3,20,3);
+            g.fillRect(6,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 6){
+            g.fillRect(12,3,20,3);
+            g.fillRect(6,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+            g.fillRect(6,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 7){
+            g.fillRect(12,3,20,3);
+            g.fillRect(35,8,3,20);
+            g.fillRect(35,33,3,20);
+        }else if(value == 8){
+            g.fillRect(12,3,20,3);
+            g.fillRect(6,8,3,20);
+            g.fillRect(35,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+            g.fillRect(6,33,3,20);
+            g.fillRect(12,55,20,3);
+        }else if(value == 9){
+            g.fillRect(12,3,20,3);
+            g.fillRect(6,8,3,20);
+            g.fillRect(35,8,3,20);
+            g.fillRect(12,30,20,3);
+            g.fillRect(35,33,3,20);
+            g.fillRect(12,55,20,3);
+        }
     }
 }
